b6b7e350efecf8d3548e48cbc9ba0fe18f156057
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-07 20:57:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(speaking)
   if speaking == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif speaking == speaking.upcase
-    "NO, NOT SINCE 1938!" 
+    "NO, NOT SINCE 1938!"
   else
       "HUH?! SPEAK UP, SONNY!"
   end
