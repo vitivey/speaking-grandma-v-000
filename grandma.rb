08d537867567b4909bfef71125f42534ca42c059
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-07 20:52:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speaking)
-  if speaking != speaking.uppercase
+  if speaking != speaking.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif speaking == speaking.uppercase
+  elsif speaking == speaking.upcase
     puts "NO, NOT SINCE 1938!"
   elsif speaking == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
